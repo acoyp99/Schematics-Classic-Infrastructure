9dbb03f7dd1f78c3d9582c24b62e235c75d0934d
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-04-03 17:05:33 -0500

    Update variables.tf

--- vsphere1/variables.tf
+++ vsphere1/variables.tf
@@ -1,3 +1,17 @@
+variable ibmcloud_apikey {
+  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+}
+
+variable ibm_region {
+  description = "IBM Cloud region where all resources will be deployed"
+  default     = "us-south"
+}
+
+variable resource_group {
+  description = "Name of resource group to provision resources"
+  default     = "terraform"
+}
+
 variable "vsphere_server" {
   description = "vsphere server to connect to"
   default     = "___INSERT YOUR OWN____"
